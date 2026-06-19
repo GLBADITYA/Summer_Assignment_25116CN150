7cae5d7bf3ed5a61352d5b201f6105f3b4e80ffd
ADITYA SAXENA <adityasaxena2xz@gmail.com>
2026-06-19 14:52:38 +0530

    DAY 17 ASSIGNMENT ADDED

---
+++ Day-17/Q66.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"enter size of both array :";
+    cin>>n>>m;
+    int a[n],b[m];
+    cout<<"enter elements in array 1:\n";
+    for(int i =0;i<n;i++){
+        cin>>a[i];
+    }
+     cout<<"enter elements in array 2:\n";
+    for(int i =0;i<m;i++){
+        cin>>b[i];
+    }
+    int c[n+m],k=0;
+    for(int i=0;i<n;i++){
+        c[k++]=a[i];
+    }
+    for(int i = 0 ;i < m ; i++){
+        bool f = false;
+        for(int j =0 ;j<n; j++){
+            if(b[i]==a[j]){
+                f=true;
+                break;
+            }
+        }
+        if(!f){
+         c[k++]=b[i];
+        }
+        
+    }
+     for(int i=0;i<k;i++){
+        cout<<c[i]<<" ";
+        
+    }
+    return 0;
+}
